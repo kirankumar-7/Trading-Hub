c51b9a14366cbcae6ddf61a5e5df7d030e962b71
kirankumar-7 <kiransep97@gmail.com>
2025-06-19 23:57:47 +0530

    Change file paths in app.py

--- candlestick-screener/app.py
+++ candlestick-screener/app.py
@@ -3,7 +3,6 @@ import json
 import talib
 import pandas as pd
 from flask import Flask, request, render_template
-from strategies.indicators import pivot_levels
 from binance.client import Client
 import psycopg2
 
@@ -18,52 +17,15 @@ app = Flask(__name__)
 
 client = Client()
 
-start_str = '1 Jan, 2024'
-end_str = '16 Nov, 2024'
-interval = '1d'
-
 @app.route('/')
 def home():
     return f'''
         <h1>Welcome to Crypto Candle Stick Screener</h1>
         <p>Select an option below:</p>
         <ul>
-            <li><a href="/snapshot">Snapshot</a> - Download all historical Binance Crypto data in a CSV file.</li>
             <li><a href="/patternDetect">Pattern Detection</a> - Run the candlestick pattern screener.</li>
         </ul>
     '''
-
-@app.route('/snapshot')
-def snapshot():
-    with open('datasets/crypto_usdt_symbols.csv', 'r') as f:
-        for symbol in f:
-            symbol = symbol.strip()
-            try:
-                data = client.get_historical_klines(symbol, interval, start_str, end_str)
-
-                processed_data = []
-                for x in data:
-                    row = [
-                        datetime.fromtimestamp(x[0] / 1000).strftime('%Y-%m-%d'),  #timestamp to Date
-                        float(x[1]),  # Open
-                        float(x[2]),  # High
-                        float(x[3]),  # Low
-                        float(x[4]),  # Close
-                        float(x[5]),  # Volume
-                        int(x[8])     # Number of trades
-                    ]
-                    processed_data.append(row)
-
-                df = pd.DataFrame(processed_data, columns=[
-                        'Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Number of trades'
-                        ])
-                
-                df.to_csv(f'datasets/crypto_usdt_daily_data/{symbol}.csv', index=False)
-                print('{} Data loaded to csv file successfully'.format(symbol))
-            except Exception as e:
-                print(f"Failed to fetch data for {symbol}: {e}")
-    
-    return 'Data Extrated from binance API for the given symbols successfully'
 
 @app.route('/patternDetect')
 def index():
@@ -117,16 +79,6 @@ def index():
 
     print(crypto)
     return render_template('index.html', candlestick_patterns=candlestick_patterns, crypto=crypto, pattern=pattern)
-    
-@app.route('/predict', methods=['POST'])  # For testing via Postman
-def predict():
-    if request.method == 'POST':
-        high = float(request.json['high'])
-        low = float(request.json['low'])
-        close = float(request.json['close'])
-
-        result_json = pivot_levels(high, low, close)
-        return jsonify(json.loads(result_json))
 
 if __name__ == '__main__':
     app.run(debug=True)
